25ce4d7fdb31c53d9fb7ef8466408a8f29ff6cc7
jinzhengkai <1550827517@qq.com>
2022-08-01 19:16:50 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,100 @@
-import React from 'react'
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React from 'react';
+ import 'antd/dist/antd.min.css'
 
-export default function app() {
-  return (
-    <h1>app</h1>
-  )
-}
+const { Header, Content, Footer, Sider } = Layout;
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+  const key = String(index + 1);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+
+const App = () => (
+  <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+    </Header>
+    <Content
+      style={{
+        padding: '0 50px',
+      }}
+    >
+      <Breadcrumb
+        style={{
+          margin: '16px 0',
+        }}
+      >
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <Layout
+        className="site-layout-background"
+        style={{
+          padding: '24px 0',
+        }}
+      >
+        <Sider className="site-layout-background" width={200}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{
+              height: '100%',
+            }}
+            items={items2}
+          />
+        </Sider>
+        <Content
+          style={{
+            padding: '0 24px',
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Content>
+    <Footer
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      Ant Design ©2018 Created by Ant UED
+    </Footer>
+  </Layout>
+);
+
+export default App;
+// #components - layout - demo - top - side.logo {
+//   float: left;
+//   width: 120px;
+//   height: 31px;
+//   margin: 16px 24px 16px 0;
+//   background: rgba(255, 255, 255, 0.3);
+// }
+
+// .ant - row - rtl #components - layout - demo - top - side.logo {
+//   float: right;
+//   margin: 16px 0 16px 24px;
+// }
+
+// .site - layout - background {
+//   background: #fff;
+// }
